b3c1f6c553bcbb0147d420ba839d9ebb421d9470
JosseMontano <josemariazambranamontano123@gmail.com>
2024-03-10 16:08:42 -0400

    create components to the forms

--- src/Global/components/input.tsx
+++ src/Global/components/input.tsx
@@ -6,16 +6,17 @@ interface Props {
   placeholder: string;
   register: UseFormRegisterReturn;
   label:string
+  type:string
 }
 
-const Input = ({error, placeholder, register, label}: Props) => {
+const Input = ({error, placeholder, register, label, type}: Props) => {
   return (
     <div>
       <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
        {label}
       </label>
       <input
-        type="email"
+        type={type}
         className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         placeholder={placeholder}
         {...register}
